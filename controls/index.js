9404c3f4251f22d05eca56462af3e9c0b83f82c2
cesarleyton <cesarleyton549@gmail.com>
2024-07-28 16:34:54 -0500

    added function to check if the key matches the current  letter

--- controls/index.js
+++ controls/index.js
@@ -1,8 +1,10 @@
-export function onKeyPress(){
+import { mainState } from "../index.js";
 
+export function onKeyPress(e) {
+  if (e.key === mainState.currentLetter) {
+    mainState.wordsStreak++;
+    document.querySelector(".streak").textContent = mainState.wordsStreak;
+  }
 }
 
-
-export function onKeyUp(){
-
-}
+export function onKeyUp() {}
--- index.js
+++ index.js
@@ -1,19 +1,50 @@
+import { onKeyPress } from "./controls/index.js";
 
-const DEFAULT_TIME = 30
+const DEFAULT_TIME = 30;
 
-let words = []
+let words = [];
 
-let currentTime = DEFAULT_TIME
+let currentTime = DEFAULT_TIME;
 
-let paragraphEl = document.querySelector('p')
-let TEXT = "back in highschool i use to bus into the dance, now i hit the fbo with duffles in my hands"
-function startGame(){
-    paragraphEl.textContent = TEXT
+export const mainState = {
+  currentLetter: "",
+  wordsStreak: 0,
+  currentWord: "",
+};
+let paragraphEl = document.querySelector("p");
+let TEXT =
+  "back in highschool i use to bus into the dance, now i hit the fbo with duffles in my hands";
+function startGame() {
+  words = TEXT.split(" ").slice(0, 32);
+  document.querySelector(".streak").textContent = mainState.wordsStreak;
+  document.querySelector("time").textContent = currentTime;
+  paragraphEl.innerHTML = words
+    .map((w) => {
+      const letters = w.split("");
 
-
+      return `<span class="word">
+    ${letters.map((l) => `<span class="letter">${l}</span>`).join("")}
+</span>`;
+    })
+    .join("");
 }
-startGame()
 
-function startEvents(){
+function startEvents() {
+  const intervalId = setInterval(() => {
+    currentTime--;
+    document.querySelector("time").innerHTML = currentTime;
 
+    if (currentTime === 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+
+  const firstWord = document.querySelector(".word");
+  const firstLetter = document.querySelector(".letter");
+  mainState.currentLetter = firstLetter.textContent;
+  firstWord.classList.add("active");
 }
+
+document.addEventListener("keydown", onKeyPress);
+startGame();
+startEvents();
