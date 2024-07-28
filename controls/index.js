34f4b1e0283854128262dbd289810208060615ff
cesarleyton <cesarleyton549@gmail.com>
2024-07-28 14:27:13 -0500

    root commit

---
+++ controls/index.js
@@ -0,0 +1,8 @@
+export function onKeyPress(){
+
+}
+
+
+export function onKeyUp(){
+
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+
+const DEFAULT_TIME = 30
+
+let words = []
+
+let currentTime = DEFAULT_TIME
+
+let paragraphEl = document.querySelector('p')
+let TEXT = "back in highschool i use to bus into the dance, now i hit the fbo with duffles in my hands"
+function startGame(){
+    paragraphEl.textContent = TEXT
+
+
+}
+startGame()
+
+function startEvents(){
+
+}
