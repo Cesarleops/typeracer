aa999859da0ec1dc6f353452a98f4fa97492ceed
cesarleyton <cesarleyton549@gmail.com>
2024-07-31 16:15:52 -0500

    refactor

--- game/index.js
+++ game/index.js
@@ -30,50 +30,11 @@ export class Game {
     const currentLetter = currentWord[this.currentLetterIdx];
     const currentWordEl = this.allWordsEl[this.currentWordIdx];
     const currentWordLetters = currentWordEl.querySelectorAll(".letter");
-    console.log("my streak", this.streak);
 
     if (key === "Backspace") {
       //Two possible cases, the cursor is in the first letter of a word, or in any other letter.
       //When backspacing i have to remove the correct letters
-
-      const prevWord = currentWordEl.previousElementSibling;
-
-      if (this.currentWordIdx === 0 && this.currentLetterIdx === 0) {
-        console.log("first letter of first word");
-        return;
-      }
-
-      if (this.currentLetterIdx === 0 && this.currentWordIdx > 0) {
-        console.log("retrocede a una palabra anterior");
-        //Move the cursor to the last letter of the previous word
-        const pre = prevWord.querySelectorAll(".letter");
-        this.currentWordIdx--;
-        currentWordLetters[this.currentLetterIdx].classList.remove("active");
-        this.currentLetterIdx = pre.length;
-        pre[this.currentLetterIdx - 1].classList.add("last");
-
-        return;
-      }
-
-      if (this.currentLetterIdx === currentWord.length) {
-        currentWordLetters[this.currentLetterIdx - 1].classList.remove("last");
-      } else {
-        currentWordLetters[this.currentLetterIdx].classList.remove("active");
-      }
-      this.currentLetterIdx--;
-      if (this.currentLetterIdx === currentWordLetters.length) {
-        console.log("esta es la ultima letra");
-        currentWordLetters[this.currentLetterIdx - 1].classList.add("last");
-
-        return;
-      }
-      currentWordLetters[this.currentLetterIdx].classList.add("active");
-
-      currentWordLetters[this.currentLetterIdx].classList.remove(
-        "correct",
-        "incorrect"
-      );
-
+      this.handleBackSpace(currentWordLetters, currentWordEl, currentWord);
       return;
     }
 
@@ -82,34 +43,7 @@ export class Game {
     }
 
     if (key === " ") {
-      e.preventDefault();
-      document.querySelector("input").value = "";
-      if (this.currentLetterIdx < currentWord.length) {
-        currentWordLetters[this.currentLetterIdx].classList.remove("active");
-      }
-
-      if (this.currentLetterIdx === currentWord.length) {
-        currentWordLetters[this.currentLetterIdx - 1].classList.remove("last");
-      }
-      const wordHasErrors =
-        currentWordEl.querySelectorAll(".letter:not(.correct)").length > 0;
-      wordHasErrors
-        ? currentWordEl.classList.add("marked")
-        : currentWordEl.classList.remove("marked");
-
-      if (!currentWordEl.classList.contains("marked")) {
-        this.streak++;
-        document.querySelector(".streak").textContent = this.streak;
-      } else {
-        this.streak = 0;
-        document.querySelector(".streak").textContent = this.streak;
-      }
-      this.currentWordIdx++;
-      this.currentLetterIdx = 0;
-      currentWordEl.nextElementSibling
-        .querySelector(".letter")
-        .classList.add("active");
-
+      this.handleSpace(e, currentWordEl, currentWord, currentWordLetters);
       return;
     }
 
@@ -135,7 +69,73 @@ export class Game {
     currentWordLetters[this.currentLetterIdx].classList.add("active");
   }
 
-  handleBackSpace() {}
+  handleBackSpace(currentWordLetters, currentWordEl, currentWord) {
+    const prevWord = currentWordEl.previousElementSibling;
 
-  handleSpace() {}
+    if (this.currentWordIdx === 0 && this.currentLetterIdx === 0) {
+      console.log("first letter of first word");
+      return;
+    }
+
+    if (this.currentLetterIdx === 0 && this.currentWordIdx > 0) {
+      console.log("retrocede a una palabra anterior");
+      //Move the cursor to the last letter of the previous word
+      const pre = prevWord.querySelectorAll(".letter");
+      this.currentWordIdx--;
+      currentWordLetters[this.currentLetterIdx].classList.remove("active");
+      this.currentLetterIdx = pre.length;
+      pre[this.currentLetterIdx - 1].classList.add("last");
+
+      return;
+    }
+
+    if (this.currentLetterIdx === currentWord.length) {
+      currentWordLetters[this.currentLetterIdx - 1].classList.remove("last");
+    } else {
+      currentWordLetters[this.currentLetterIdx].classList.remove("active");
+    }
+    this.currentLetterIdx--;
+    if (this.currentLetterIdx === currentWordLetters.length) {
+      console.log("esta es la ultima letra");
+      currentWordLetters[this.currentLetterIdx - 1].classList.add("last");
+
+      return;
+    }
+    currentWordLetters[this.currentLetterIdx].classList.add("active");
+
+    currentWordLetters[this.currentLetterIdx].classList.remove(
+      "correct",
+      "incorrect"
+    );
+  }
+
+  handleSpace(e, currentWordEl, currentWord, currentWordLetters) {
+    e.preventDefault();
+    document.querySelector("input").value = "";
+    if (this.currentLetterIdx < currentWord.length) {
+      currentWordLetters[this.currentLetterIdx].classList.remove("active");
+    }
+
+    if (this.currentLetterIdx === currentWord.length) {
+      currentWordLetters[this.currentLetterIdx - 1].classList.remove("last");
+    }
+    const wordHasErrors =
+      currentWordEl.querySelectorAll(".letter:not(.correct)").length > 0;
+    wordHasErrors
+      ? currentWordEl.classList.add("marked")
+      : currentWordEl.classList.remove("marked");
+
+    if (!currentWordEl.classList.contains("marked")) {
+      this.streak++;
+      document.querySelector(".streak").textContent = this.streak;
+    } else {
+      this.streak = 0;
+      document.querySelector(".streak").textContent = this.streak;
+    }
+    this.currentWordIdx++;
+    this.currentLetterIdx = 0;
+    currentWordEl.nextElementSibling
+      .querySelector(".letter")
+      .classList.add("active");
+  }
 }
